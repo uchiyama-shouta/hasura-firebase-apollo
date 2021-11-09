8833cfa70d426fd9f5081b2cd2cc58fa07e67404
utiyama <shou0604pro@icloud.com>
2021-11-10 03:14:11 +0900

    SGの導入

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 import type { NextPage, GetStaticProps } from "next";
 import { Auth } from "src/components/Auth";
 import { Layout } from "src/components/Layout";
-import { initializeApollo } from "src/lib/apolloClient";
 import { GET_NEWS } from "src/queries/news/newsQueries";
 import { News } from "src/types/responseType";
 
@@ -9,9 +9,18 @@ type Props = {
 	data: News[];
 };
 
+type Res = {
+	news: News[];
+};
+
 const Home: NextPage<Props> = ({ data }) => {
 	return (
 		<Layout title="Home">
+			{data?.map((news) => (
+				<p className="font-bold" key={news.id}>
+					{news.content}
+				</p>
+			))}
 			<Auth />
 		</Layout>
 	);
@@ -19,3 +28,19 @@ const Home: NextPage<Props> = ({ data }) => {
 
 export default Home;
 
+export const getStaticProps: GetStaticProps = async () => {
+	const client = new ApolloClient({
+		uri: process.env.NEXT_PUBLIC_HASURA_URL,
+		cache: new InMemoryCache(),
+	});
+	const { data } = await client.query<Res>({
+		query: GET_NEWS,
+	});
+
+	return {
+		props: {
+			data: data.news,
+		},
+		revalidate: 60 * 60,
+	};
+};
